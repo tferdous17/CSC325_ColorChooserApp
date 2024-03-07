8a3558838724c5ee61a1b4d126a1391e34f2a60c
Tasnim Ferdous <tferdous611@gmail.com>
2024-03-06 21:08:46 -0500

    fix: stage had wrong title

--- src/main/java/org/example/csc325_colorchooserapp/ColorChooser.java
+++ src/main/java/org/example/csc325_colorchooserapp/ColorChooser.java
@@ -12,7 +12,7 @@ public class ColorChooser extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ColorChooser.class.getResource("ColorChooser.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Color Chooser");
         stage.setScene(scene);
         stage.show();
     }
